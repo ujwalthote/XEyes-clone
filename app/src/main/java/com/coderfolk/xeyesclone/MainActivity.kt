ca1e835228fe248b2ebd3c589ca9969be8059d2c
Ujwal Thote <ujwal.thote@coditas.com>
2019-06-11 14:06:34 +0530

    Initialise the view objects

--- app/src/main/java/com/coderfolk/xeyesclone/MainActivity.kt
+++ app/src/main/java/com/coderfolk/xeyesclone/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.coderfolk.xeyesclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var vgRootContainer: RelativeLayout? = null
+    private var vgContainerEye: RelativeLayout? = null
+    private var viewEyeBall: View? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        vgRootContainer = findViewById(R.id.vgRootContainer)
+        vgContainerEye = findViewById(R.id.vgContainerEye)
+        viewEyeBall = findViewById(R.id.viewEyeBall)
     }
 }
+
+
