8153b70337b83016d8b6d4a65688b74cdd6f1967
Ujwal Thote <ujwal.thote@coditas.com>
2019-06-11 14:28:59 +0530

    Increase the width of the circle Keep the backup of the center of the container view

--- app/src/main/java/com/coderfolk/xeyesclone/MainActivity.kt
+++ app/src/main/java/com/coderfolk/xeyesclone/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.coderfolk.xeyesclone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.RelativeLayout
@@ -10,8 +11,8 @@ class MainActivity : AppCompatActivity() {
     private var vgRootContainer: RelativeLayout? = null
     private var vgContainerEye: RelativeLayout? = null
     private var viewEyeBall: View? = null
-
-
+    private var xOfEyeContainer: Float? = null
+    private var yOfEyeContainer: Float? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +23,24 @@ class MainActivity : AppCompatActivity() {
         vgRootContainer = findViewById(R.id.vgRootContainer)
         vgContainerEye = findViewById(R.id.vgContainerEye)
         viewEyeBall = findViewById(R.id.viewEyeBall)
+        keepTheCenterOfVgEyeContainer()
+        setUpListener()
     }
+
+    private fun keepTheCenterOfVgEyeContainer() {
+        xOfEyeContainer = vgContainerEye?.pivotX
+        yOfEyeContainer = vgContainerEye?.pivotY
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setUpListener() {
+        vgRootContainer?.setOnTouchListener { v, event ->
+
+            return@setOnTouchListener v?.onTouchEvent(event) ?: true
+        }
+    }
+
 }
+
 
 
